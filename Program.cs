d24bd6d892547c005d9b5000a04353f71a5aba1e
Lucas Öhrn <lucas.ohrn@codeitab.se>
2020-09-24 13:32:25 +0200

    Ändrat output

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MineSweeper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Said gillar snus");
+        }
+    }
+}
